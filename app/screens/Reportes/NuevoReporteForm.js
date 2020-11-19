f967112ea69e3894b75cfa47e0959a61c6331cf9
csomeragmn <csomera@gamansolutions.mx>
2020-11-19 03:59:27 -0700

    catalogo reportes listado crear y editar

--- app/screens/Reportes/NuevoReporteForm.js
+++ app/screens/Reportes/NuevoReporteForm.js
@@ -1,123 +1,198 @@
-import React,{ useState, useRef} from "react";
-import {StyleSheet, View, Text, ScrollView, Alert, Dimensions, Picker} from "react-native";
-import {Icon, Avatar, Image, Input, Button} from "react-native-elements";
+import React, { useState, useRef } from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    ScrollView,
+    Alert,
+    Dimensions,
+    Picker,
+} from "react-native";
+import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 import Modal from "../../components/Modal";
 
-export default function NuevoReporteForm(props){
-    const[isVisibleMap, setIsVisibleMap] = useState(false);
-    return(
+export default function NuevoReporteForm(props) {
+    const [isVisibleMap, setIsVisibleMap] = useState(false);
+
+    return (
         <ScrollView style={StyleSheet.ScrollView}>
             <FormAdd
+                mascota={props.mascota}
+                setAnimales={props.setAnimales}
                 setIsVisibleMap={setIsVisibleMap}
+                navigation={props.navigation}
             />
-            <UploadImage/>
+            <UploadImage />
             <Button
                 title="Reportar"
                 containerStyle={styles.containerReportar}
                 buttonStyle={styles.btnReportar}
                 style={{ margin: "15px" }}
+                onPress={() => {
+                    console.log("llegoHastaBoton");
+                    setAnimales(mascota);
+                }}
             />
-            <Map isVisibleMap={isVisibleMap} setIsVisibleMap={setIsVisibleMap}/>
+            <Map isVisibleMap={isVisibleMap} setIsVisibleMap={setIsVisibleMap} />
         </ScrollView>
     );
 }
 
+function FormAdd(props) {
+    console.log("PROPS");
+    console.log(props);
+    const { setAnimales, navigation } = props;
+    const [mascota, setMascota] = useState(
+        !props.mascota
+            ? {
+                tipoReporteID: -1,
+                EspecieID: -1,
+                TamanoID: -1,
+                ColorID: -1,
+                Direccion: "",
+            }
+            : props.mascota
+    );
 
-function FormAdd(props){
-    const{
-        setIsVisibleMap
-     } = props;
-    return(
-        <View style = {styles.viewForm}>
+
+    const { setIsVisibleMap } = props;
+    console.log("MASCOTA");
+    console.log(mascota);
+
+
+    const onChange = (e) => {
+        console.log(e);
+        console.log(e.target.name);
+        console.log(e.target.value);
+        mascota[e.target.name] = e.target.value;
+        setMascota(mascota);
+    }
+
+    return (
+        <View style={styles.viewForm}>
             <Input
+                value={mascota.name}
                 placeholder="Nombre de la mascota"
                 containerStyle={styles.input}
+                onChange={
+                    (e) => {
+                        mascota.name = e.target.value
+                        setMascota({ ...mascota });
+                    }
+                }
             />
-            <Text>
-                Tipo de Reporte:
-            </Text>
+            <Text>Tipo de Reporte:</Text>
             <Picker
-            style={{height: 30, width: "100%"}}
-            onValueChange={(itemValue, itemIndex) =>
-            {}                      
-            }>
-                <Picker.Item label="Extravío" value="extravio" />
-                <Picker.Item label="Calle" value="calle" />
+                style={{ height: 30, width: "100%" }}
+                value={mascota.tipoReporteID}
+                onValueChange={(itemValue, itemIndex) => {
+                    console.log(itemValue);
+                    mascota.tipoReporteID = itemValue;
+                    setMascota({ ...mascota });
+                    console.log(mascota);
+                }
+                }>
+
+                <Picker.Item label="Extravío" value={1} />
+                <Picker.Item label="Calle" value={2} />
             </Picker>
-            <Text>
-                Especie:
-            </Text>
+            <Text>Especie:</Text>
             <Picker
-            style={{height: 30, width: "100%"}}
-            onValueChange={(itemValue, itemIndex) =>
-            {}
-            }>
-                <Picker.Item label="Perro" value="perro" />
-                <Picker.Item label="Gato" value="gato" />
-                <Picker.Item label="Conejo" value="conejo" />
-                <Picker.Item label="Hurón" value="huron" />
+                style={{ height: 30, width: "100%" }}
+                onValueChange={(itemValue, itemIndex) => {
+                    mascota.EspecieID = itemValue
+                    setMascota({ ...mascota });
+                }}
+                selectedValue={mascota.EspecieID}
+                name={"EspecieID"}
+
+            >
+                <Picker.Item label="Perro" value={1} />
+                <Picker.Item label="Gato" value={2} />
+                <Picker.Item label="Conejo" value={3} />
+                <Picker.Item label="Hurón" value={4} />
             </Picker>
-            <Text>
-                Tamaño:
-            </Text>
+            <Text>Tamaño:</Text>
             <Picker
-            style={{height: 30, width: "100%"}}
-            onValueChange={(itemValue, itemIndex) =>
-            {}
-            }>
-                <Picker.Item label="Chico" value="chico" />
-                <Picker.Item label="Mediano" value="mediano" />
-                <Picker.Item label="Grande" value="grande"/>
+                selectedValue={mascota.TamanoID}
+                style={{ height: 30, width: "100%" }}
+                onValueChange={(itemValue, itemIndex) => {
+                    mascota.TamanoID = itemValue
+                    setMascota({ ...mascota });
+                }}
+                name={"TamanoID"}
+            >
+                <Picker.Item label="Chico" value={1} />
+                <Picker.Item label="Mediano" value={2} />
+                <Picker.Item label="Grande" value={3} />
             </Picker>
-            <Text>
-                Color:
-            </Text>
+            <Text>Color:</Text>
             <Picker
-            style={{height: 30, width: "100%"}}
-            onValueChange={(itemValue, itemIndex) =>
-            {}
-            }>
-                <Picker.Item label="Café" value="cafe" />
-                <Picker.Item label="Negro" value="negro" />
-                <Picker.Item label="Blanco" value="blanco" />
-                <Picker.Item label="Amarillo" value="amarillo" />
-                <Picker.Item label="Otro" value="otro" />
+                selectedValue={mascota.ColorID}
+                style={{ height: 30, width: "100%" }}
+                onValueChange={(itemValue, itemIndex) => {
+                    mascota.ColorID = itemValue
+                    setMascota({ ...mascota });
+                }}
+                name={"ColorID"}
+
+            >
+                <Picker.Item label="Café" value={1} />
+                <Picker.Item label="Negro" value={2} />
+                <Picker.Item label="Blanco" value={3} />
+                <Picker.Item label="Amarillo" value={4} />
+                <Picker.Item label="Otro" value={5} />
             </Picker>
             <Input
                 placeholder="Descripción"
                 containerStyle={styles.input}
+                value={mascota.Descripcion}
+                onChange={
+                    (e) => {
+                        mascota.Descripcion = e.target.value
+                        setMascota({ ...mascota });
+                    }
+                }
+                name={"Descripcion"}
+
             />
             <Input
                 placeholder="Dirección"
                 containerStyle={styles.input}
-                rightIcon={{
-                    type: "material-community",
-                    name: "google-maps",
-                    color:"#c2c2c2",
-                    onPress: () => setIsVisibleMap(true),
-                }}
+                value={mascota.Dir}
+                onChange={
+                    (e) => {
+                        mascota.Dir = e.target.value
+                        setMascota({ ...mascota });
+                    }
+                }
+                name={"Dir"}
+
             />
             <Button
-                title="Reportar"
+                onPress={() => {
+                    console.log("llegoHastaBoton");
+                    setAnimales(mascota);
+                    navigation.navigate("reportes");
+                }}
+                title={mascota.index >= 0 ? "Guardar" : "Reportar"}
                 buttonStyle={styles.btnReportar}
-            />  
+            />
         </View>
-    )
+    );
 }
 
-function UploadImage(){
+function UploadImage() {
     const ImageSelect = async () => {
         const resultPermissions = await Permissions.askAsync(
             Permissions.CAMERA_ROLL
         );
         console.log(resultPermissions);
     };
-    return(
-        <View
-            style={styles.viewImages}
-        >
+    return (
+        <View style={styles.viewImages}>
             <Icon
                 type="material-community"
                 name="camera"
@@ -126,22 +201,18 @@ function UploadImage(){
                 onPress={ImageSelect}
             />
         </View>
-    )
-}
-
-function Map(props){
-    const {isVisibleMap, setIsVisibleMap} = props;
-
-    return(
-        <Modal isVisible ={isVisibleMap} setIsVisible={setIsVisibleMap}>
-            <Text>
-                Mapa
-            </Text>
-        </Modal>
     );
 }
 
+function Map(props) {
+    const { isVisibleMap, setIsVisibleMap } = props;
 
+    return (
+        <Modal isVisible={isVisibleMap} setIsVisible={setIsVisibleMap}>
+            <Text>Mapa</Text>
+        </Modal>
+    );
+}
 
 const styles = StyleSheet.create({
     scrollView: {
@@ -151,8 +222,8 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         marginRight: 10,
     },
-    input:{
-        marginBottom:10,
+    input: {
+        marginBottom: 10,
     },
     containerReportar: {
         marginTop: 40,
@@ -162,7 +233,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#FF7E00",
         width: "100%",
     },
-    viewImages:{
+    viewImages: {
         flexDirection: "row",
         marginLeft: 20,
         marginRight: 20,
--- app/screens/Reportes/Reportes.js
+++ app/screens/Reportes/Reportes.js
@@ -1,23 +1,109 @@
-import React from "react";
-import {StyleSheet, View, Text} from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, ScrollView, Text, View, TouchableOpacity, Image } from "react-native";
 import { Icon } from "react-native-elements";
+import { ListItem, Avatar } from 'react-native-elements'
 
-export default function Reportes(props){
-    const{navigation} = props;
-    return(
-        <View style={styles.viewBody}>
-            <Text>
-                Reportes...
-            </Text>
-            <Icon
-                reverse
-                type="material-community"
-                name="plus"
-                color="#EDB506"
-                containerStyle = {styles.btnContainer}
-                onPress = {() => navigation.navigate("nuevo_reporte")}
-            />
-        </View>
+export default function Reportes(props) {
+    const { navigation } = props;
+
+    const Africa = {
+        index: 0,
+        name: 'Africa',
+        avatar_url: 'https://www.purina.com.au/-/media/project/purina/main/breeds/dog/dog_belgian-shepherd-dog-malinois_desktop.jpg?h=475&la=en&w=825&hash=4453F39A31F8404DF998D772EDA8D5AA',
+        subtitle: 'Perdida',
+        date: '12 de noviembre de 2020',
+
+        tipoReporteID: 1,
+        EspecieID: 1,
+        TamanoID: 3,
+        ColorID: 1,
+        Dir: 'Bahia de ohuira #666 colonia morelos',
+        Descripcion: 'Se escapo cerca de la papeleria',
+
+    }
+
+    const Ringo = {
+        index: 1,
+        name: 'Perro feo de la esquina',
+        avatar_url: 'https://instagram.fcul2-1.fna.fbcdn.net/v/t51.2885-15/e35/47691534_1981288845508250_3304396044808224768_n.jpg?_nc_ht=instagram.fcul2-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=C5kRCFH8rcoAX-w93ys&tp=18&oh=1cb784d003c63617ce70cf0e263a4e2f&oe=5FDF16F7',
+        subtitle: 'Visto',
+        date: '28 de marzo de 2020',
+
+        tipoReporteID: 2,
+        EspecieID: 1,
+        TamanoID: 2,
+        ColorID: 4,
+        Dir: 'Puerto de ringo #333 colonia nvo culiacan',
+        Descripcion: 'Lo vi cerca de la casa de una vecina, me parece que le esta dando comida y agua',
+
+    }
+
+
+    const [mascotas, setMascotas] = useState([Africa, Ringo]);
+
+    const setAnimales = (nuevaMascota) => {
+        const copia = [...mascotas]
+        if (nuevaMascota.index >= 0) {
+            console.log("index");
+            copia[nuevaMascota.index] = nuevaMascota;
+
+        }
+        else {
+            console.log("NO index");
+            nuevaMascota.date = "19 de noviembre del 2020"
+            nuevaMascota.index = copia.length;
+            copia.push(nuevaMascota);
+        }
+        setMascotas([...copia]);
+    }
+
+    useEffect(() => {
+        console.log("useeffect");
+        return () => {
+        }
+    })
+
+    return (
+        <>
+            <ScrollView style={styles.viewBody}>
+                {
+                    mascotas.map((l, i) => (
+                        <ListItem key={i} bottomDivider>
+                            <Avatar source={{ uri: l.avatar_url }} />
+                            <ListItem.Content>
+                                <ListItem.Title><span style={{ fontWeight: "500" }}>{l.name}</span></ListItem.Title>
+                                <ListItem.Subtitle>{l.tipoReporteID === 1 ? "Perdido" : "Visto"}</ListItem.Subtitle>
+                                <ListItem.Subtitle>{l.date}</ListItem.Subtitle>
+                            </ListItem.Content>
+                            <Avatar onPress={() => navigation.navigate("nuevo_reporte"
+                                , { mascota: l, setAnimales }
+                            )}
+                                source={{
+                                    uri:
+                                        'http://www.myiconfinder.com/uploads/iconsets/256-256-4aff514fe3d4cdea16cedd424851d5f7-arrow.png'
+                                }}
+                            />
+
+
+                        </ListItem>
+                    ))
+                }
+
+
+            </ScrollView>
+            <TouchableOpacity>
+                <Icon
+                    reverse
+                    type="material-community"
+                    name="plus"
+                    color="#EDB506"
+                    containerStyle={styles.btnContainer}
+                    onPress={() => navigation.navigate("nuevo_reporte"
+                        , { setAnimales }
+                    )}
+                />
+            </TouchableOpacity>
+        </>
     );
 }
 
@@ -29,9 +115,10 @@ const styles = StyleSheet.create({
     btnContainer: {
         position: "absolute",
         bottom: 10,
-        right: 10,
+        right: 25,
         shadowColor: "black",
-        shadowOffset: {width:2, height:2},
+        shadowOffset: { width: 2, height: 2 },
         shadowOpacity: 0.3,
+
     },
 })
